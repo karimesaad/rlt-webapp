ba98ba4b0a41154cf72bef40cfedd709a8da387b
Blake <muirblake@yahoo.com>
2017-10-02 16:47:24 -0500

    Button to edit lessons: COMPLETE

--- app/scripts/controllers/editsublesson.js
+++ app/scripts/controllers/editsublesson.js
@@ -8,10 +8,46 @@
  * Controller of the testappApp
  */
 angular.module('testappApp')
-  .controller('EditsublessonCtrl', function () {
+  .controller('EditsublessonCtrl', function ($scope, firebaseFactory, $firebaseArray, $firebase, $firebaseObject) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    var editSequencer = new Nexus.Sequencer('#sequencer',{
+      'size': [500,500],
+      'mode': 'toggle',
+      'rows': 4,
+      'columns': 4
+    });
+    var startButton = new Nexus.Button('#start-button',{
+      'id': 'circle-svg',
+      'size': [60,60],
+      'mode': 'toggle',
+      'state': false
+    });
+    startButton.on('change', function(v){
+      pushSequencer();
+    });
+    function pushSequencer() {
+      var values = [];
+
+
+      for (var i = 0; i < 4; i++){
+        for (var j = 0; j < 4; j++){
+
+           values.push(editSequencer.matrix.pattern[i][j]);
+
+        }
+      }
+      var arrayPtr = 0;
+      for (var i = 1; i < 17; i++){
+
+  firebase.database().ref('/lessons/lesson1/sublesson1/stepsequencer1/btn' + i).set({
+    hit: values[arrayPtr]
+  });
+  arrayPtr++;
+}
+console.log(values);
+}
   });
