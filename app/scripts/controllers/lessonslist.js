55c11c3bdc1ccd06b47d92a1f423c7ae0868fa1e
Matt D <matthewdaumas@utexas.edu>
2017-10-09 15:12:14 -0500

    all the stuff

--- app/scripts/controllers/lessonslist.js
+++ app/scripts/controllers/lessonslist.js
@@ -8,10 +8,32 @@
  * Controller of the testappApp
  */
 angular.module('testappApp')
-  .controller('LessonslistCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('LessonslistCtrl', function($scope, firebaseFactory, $firebaseArray, $firebase, $firebaseObject){
+    var ref = firebase.database().ref();
+    var lessonRef = 42;
+    $scope.data = $firebaseObject(ref);
+
+    $scope.data.$loaded()
+      .then(function() {
+        lessonRef = $scope.data.lessons;
+      } )
+      .catch(function(err) {
+        console.error(err);
+      });
+
+    $scope.showPageContent = function(lesson){
+      switch(lesson) {
+        case 1:
+          $scope.lesson = lessonRef.lesson1.name;
+          console.log("lesson 1");
+          break;
+        case 2:
+          $scope.lesson =2;
+          console.log("lesson 2");
+          break;
+        default:
+          console.log("lesson 0");
+          break;
+      }
+    }
   });
